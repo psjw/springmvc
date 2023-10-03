cc2175fb72395d19b1b9dd00b98b42e9e0f905bc
psjw <dlrudtn108@gmail.com>
2023-10-03 17:53:27 +0900

    main

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,8 +2,10 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 @RestController
 public class MappingController {
@@ -13,6 +15,105 @@ public class MappingController {
 //    @RequestMapping({"/hello-basic","hello-go"})
     public String helloBasic(){
         log.info("helloBasic");
+        return "ok";
+    }
+
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1(){
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 펼리한 축약 어노테이션(코드보기)
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2(){
+        log.info("mappingGetV2");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략가능
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     * /mapping/userA
+     */
+    @GetMapping("/mapping/{userId}")
+//    public String mappingPath(@PathVariable("userId") String data){
+    public String mappingPath(@PathVariable String userId){
+        log.info("mappingPath userId={}",userId);
+        return "OK";
+    }
+
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId){
+        log.info("mappingPath userId={}, orderId={}",userId, orderId);
+        return "OK";
+    }
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug" (! =)
+     * params = {"mode=debug","data=good"}
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug") //parameter에 mode=debug가 있어야함
+    public String mappingParam(){
+        log.info("mappingParam");
+        return "OK";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode",
+     * headers="!mode"
+     * headers="mode=debug"
+     * headers="mode!=debug" (! =)
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug") //parameter에 mode=debug가 있어야함
+    public String mappingHeader(){
+        log.info("mappingHeader");
+        return "OK";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE) //consume 요청의 Content-Type 정보를 소비
+    public String mappingConsumes(){
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 추가 매핑 Media Type
+     * produces="text/html"
+     * produces="!text/html"
+     * produces="text/*"
+     * produces="*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE) //produces 생산해 내는 Content-Type -> HTTP요청이 Accept랑 맞아야함
+    public String mappingProduces(){
+        log.info("mappingProduces");
         return "ok";
     }
 }
